55daa83b68577b76c091454d7b9f1a31fb5d779b
Ayomide Adeniyi <keemeayo@hotmail.com>
2025-01-14 06:34:34 +0100

    modified:   src/muTupleEnums.ts 	modified:   src/myUnion.ts

--- src/muTupleEnums.ts
+++ src/muTupleEnums.ts
@@ -1,14 +1,14 @@
-// const user: (string | number)[] = [1, "hc"]
-const tUser: [string, number, boolean]
+// // const user: (string | number)[] = [1, "hc"]
+// const tUser: [string, number, boolean]
 
-tUser = ['hc', 25, true]
+// tUser = ['hc', 25, true]
 
-let rgb: [number, number, number] = [123, 456, 789]
+// let rgb: [number, number, number] = [123, 456, 789]
 
 
-// Tuple
-type User = [number, string]
+// // Tuple
+// type User = [number, string]
 
-const newUser: User = [123, "ayo@gmail.com"]
+// const newUser: User = [123, "ayo@gmail.com"]
 
-newUser[1] = "hc.com"
+// newUser[1] = "hc.com"
--- src/myUnion.ts
+++ src/myUnion.ts
@@ -1,28 +1,28 @@
-let score: number | string = 10
+// let score: number | string = 10
 
-score = 44
+// score = 44
 
-score = '100'
+// score = '100'
 
-type User = {
-    name: string
-    age: number
-}
+// type User = {
+//     name: string
+//     age: number
+// }
 
-type Admin = {
-    username: string
-    id: number
-}
-
-
-let Ayo: User | Admin = {name: 'Ayo', age: 257}
-
-Ayo = {username: 'hggs', id: 257}
+// type Admin = {
+//     username: string
+//     id: number
+// }
 
 
-function getDbId(id: number | string) {
-    //Making soome API calls
-    console.log(`Db id is: ${id}`);  
-}
-getDbId(123)
-getDbId('234')
+// let Ayo: User | Admin = {name: 'Ayo', age: 257}
+
+// Ayo = {username: 'hggs', id: 257}
+
+
+// function getDbId(id: number | string) {
+//     //Making soome API calls
+//     console.log(`Db id is: ${id}`);  
+// }
+// getDbId(123)
+// getDbId('234')
